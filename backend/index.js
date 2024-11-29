dc0f4b71d5e2a9c81ad6bd483b2aa284addc85d0
Đỗ Lê Hậu <peotoxic3009@gmail.com>
2024-11-29 23:15:15 +0700

    backup

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 
 const config = require(("./config.json"));
-const mongoose = require ("mongoose");A
+const mongoose = require ("mongoose");
 
-mongoose.connect('mongodb+srv://testuser:testuser123@notesapp.xktcq.mongodb.net/?retryWrites=true&w=majority&appName=notesapp');
+mongoose.connect(config.connectionString);
 
 const User = require("./models/user.model");
 const Note = require("./models/note.model");
